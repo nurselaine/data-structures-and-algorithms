9a8df8e5933a3e75aaf7b53315f4c44aaca03442
nurselaine <Elaine.t.huynh@gmail.com>
2022-10-13 06:08:37 +0000

    added fizzbuzz challenge and new class for k-ary node and tree

--- javascript/code-challenges/trees/trees.test.js
+++ javascript/code-challenges/trees/trees.test.js
@@ -11,7 +11,7 @@ Returns true	false for the contains method, given an existing or non-existing no
 */
 'use strict';
 
-const { BinaryTree, BinarySearchTree, Node } = require('./tree');
+const { BinaryTree, BinarySearchTree, Node, KaryNode, KaryTree } = require('./tree');
 
 let binaryTreeTest = new BinaryTree();
 binaryTreeTest.root = new Node(10);
@@ -28,6 +28,24 @@ binarySearchTestTree.root.right = new Node(15);
 binarySearchTestTree.root.left.left = new Node(1);
 binarySearchTestTree.root.left.right = new Node(8);
 binarySearchTestTree.root.right.right = new Node(17);
+
+
+let kTreeTest = new KaryTree(10);
+
+kTree.root.children.push(5);
+kTree.root.children[0].parent = kTree;
+
+kTree.root.children.push(8);
+kTree.root.children[0].parent = kTree;
+
+kTree.root.children.push(9);
+kTree.root.children[0].parent = kTree;
+
+kTree.root.children[0].children.push(new KaryNode(7));
+kTree.root.children[0].children[0].parent = kTree.root.children[0];
+
+kTree.root.children[0].children.push(new KaryNode(6));
+kTree.root.children[0].children[1].parent = kTree.root.children[0];
 
 describe('Tree Validation', () => {
   it('Can successfully instantiate an empty tree', () => {
@@ -85,6 +103,42 @@ describe('Tree Validation', () => {
   it('Binary Tree method returns the max value in the tree', () => {
     let response = binaryTreeTest.maxValue();
 
-    expect(response).not.toBeTruthy(17);
+    expect(response).toEqual(17);
   });
+
+  it('Can successfully create a K-ary tree', () => {
+    let kTree = new KaryTree(10);
+
+    kTree.root.children.push(5);
+    kTree.root.children[0].parent = kTree;
+
+    kTree.root.children.push(8);
+    kTree.root.children[1].parent = kTree;
+
+    kTree.root.children.push(9);
+    kTree.root.children[2].parent = kTree;
+
+    kTree.root.children[0].children.push(new KaryNode(7));
+    kTree.root.children[0].children[0].parent = kTree.root.children[0];
+    
+    kTree.root.children[0].children.push(new KaryNode(6));
+    kTree.root.children[0].children[1].parent = kTree.root.children[0];
+
+
+    expect(kTree.root.value).toBe(10);
+    expect(kTree.root.children.length).toEqual(3);
+    expect(kTree.root.children[0].length).toEqual(2);
+  });
+
+  it('Handles proper fizzbuzz assignments in a K-ary tree', () => {
+    kTreeTest.fizzBuzzTree();
+
+    expect(kTreeTest.root.value).toEqual('Buzz');
+    expect(kTreeTest.root.children[0].value).toEqual('Buzz');
+    expect(kTreeTest.root.children[1].value).toEqual('8');
+    expect(kTreeTest.root.children[2].value).toEqual('Fizz');
+    expect(kTreeTest.root.children[0].children[0].value).toEqual('7');
+    expect(kTreeTest.root.children[0].children[1].value).toEqual('Fizz');
+  });
+
 });
